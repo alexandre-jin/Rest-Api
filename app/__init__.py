911f14603488e4d00d229d0a55ba2e7922dc68f1
alexandre-jin <112950317+alexandre-jin@users.noreply.github.com>
2024-06-10 14:55:14 +0200

    readme updated

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,20 @@ def create_app():
     app = Flask(__name__)
     CORS(app)
     
+    # local test
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_USER'] = 'root'
     app.config['MYSQL_PASSWORD'] = 'test123'
     app.config['MYSQL_DB'] = 'ecommerce'
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+    # global test 
+    # app.config['MYSQL_HOST'] = 'votre-serveur-mariadb' 
+    # app.config['MYSQL_PORT'] = 3306  # Le port par défaut de MariaDB est 3306
+    # app.config['MYSQL_USER'] = 'votre-utilisateur' 
+    # app.config['MYSQL_PASSWORD'] = 'votre-mot-de-passe'
+    # app.config['MYSQL_DB'] = 'Nom_de_la_DB'  
+    # app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
     
     mysql.init_app(app)
     
